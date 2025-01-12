7bf614549d30ecaec3307e93ab6aeec37b899985
Orrin19 <ofandomuno@gmail.com>
2025-01-13 03:16:28 +0700

    app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import "./App.scss";
+import { FaGithub } from "react-icons/fa";
 
 const App = () => {
   return (
     <>
-      <div></div>
+      <main className="s2catalog">
+        <h1>S.T.A.L.K.E.R. 2 Catalog</h1>
+      </main>
+      <footer className="footer">
+        <p className="footer-text">
+          S.T.A.L.K.E.R. 2 Catalog
+          {new Date().getFullYear() == 2025
+            ? " (2025"
+            : " (2025–" + new Date().getFullYear()}
+          {") | "}{" "}
+          <a href="https://github.com/Orrin19/s2catalog" target="_blank">
+            <FaGithub />
+          </a>
+        </p>
+      </footer>
     </>
   );
 };
